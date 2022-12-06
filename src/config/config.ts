17b316c1e73de7a226d4f3982862a55e41dd16f8
Max Williams <max@welcometomonday.com>
2022-12-05 21:03:39 -0800

    Added a few more config items to config helper class

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,11 +3,19 @@ const config =
 	auth:
 	{
 		clientSecret: process.env.FRIES_REST_CLIENT_SECRET!
+	},
+
+	db:
+	{
+		url: process.env.FRIES_REST_MONGO_URL!,
+		name: process.env.FRIES_REST_MONGO_NAME || 'fries-rest-dev'
 	}
 }
 
-// todo: calidate presence of required config items and throw if missing
+// todo: validate presence of required config items and throw if missing
 
 export const auth = config.auth;
+
+export const db = config.db;
 
 export default config;
