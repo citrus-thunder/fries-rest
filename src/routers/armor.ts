27d296c863fe5e9fea66d8d20b72fc93259ca94f
Max Williams <max@welcometomonday.com>
2022-12-07 18:50:28 -0800

    added some boilerplate code to routers

--- src/routers/armor.ts
+++ src/routers/armor.ts
@@ -1,5 +1,17 @@
 import express, { Request, NextFunction } from 'express';
+import passport from '../config/passport-config.js';
+
+import controller from '../controllers/armor.js';
 
 const router = express.Router();
+const auth = passport.authenticate('jwt', { session: false });
+
+router.route('/new')
+	.post(auth, controller.create);
+
+router.route('/:id')
+	.get(auth, controller.read)
+	.put(auth, controller.update)
+	.delete(auth, controller.delete);
 
 export default router;
