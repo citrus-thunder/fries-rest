9a1020f984b7b334c9f78cbafd1d0e586b2b0972
Max Williams <max@welcometomonday.com>
2022-12-08 23:31:34 -0800

    Player CRUD operations are in place

--- src/controllers/player.ts
+++ src/controllers/player.ts
@@ -1,31 +1,137 @@
 import express, { Request, Response } from 'express';
+import { WithId, Document, UpdateResult, InsertOneResult, DeleteResult } from 'mongodb';
+import { ObjectID } from 'bson';
 import Debug from 'debug';
 
+import Config from '../config/config.js';
+import Client from '../util/mdb.js';
+
 const debug = Debug('fries-rest:controller:player');
+
+const client = await Client.getClient();
+const db = client.db(Config.db.name);
+
+async function createPlayer(data: object) : Promise<InsertOneResult>
+{
+	return await db.collection('players').insertOne(data);
+}
+
+async function getPlayer(playerId: number) : Promise<WithId<Document> | null>
+{
+	return await db.collection('players').findOne({userId: playerId});
+}
+
+async function updatePlayer(playerId: number, data: object) : Promise<UpdateResult>
+{
+	const options =
+	{
+		upsert: false,
+		multiple: false
+	}
+
+	return await db.collection('players').updateOne({userId: playerId}, {$set: data}, options);
+}
+
+async function deletePlayer(playerId: number) : Promise<DeleteResult>
+{
+	return await db.collection('players').deleteOne({userId: playerId})
+}
 
 export default
 {
 	create: async (req: Request, res: Response) =>
 	{
-		// todo
-		return res.status(200);
+		const playerId = parseInt(req.params.id);
+		let body: any = null;
+
+		const existingPlayer = await getPlayer(playerId);
+
+		if (existingPlayer != null)
+		{
+			res.status(409);
+			body = `Cannot create new player record: Record with ID ${playerId} already exists!`;
+		}
+		else
+		{
+			const player = await createPlayer(req.body);
+			if (player.insertedId)
+			{
+				res.status(200);
+				body = `New player record created with userId ${playerId} and Object ID ${player.insertedId}`;
+			}
+			else
+			{
+				res.status(400)
+				body = 'Error inserting new player record: Unable to insert with given request data';
+			}
+		}
+
+		return res.send(body);
 	},
 
 	read: async (req: Request, res: Response) =>
 	{
-		// todo
-		return res.status(200);
+		const playerId = parseInt(req.params.id);
+		const player = await getPlayer(playerId);
+		let body: any = null;
+
+		if (player == null)
+		{
+			res.status(404);
+			body = `Player with ID ${playerId} not found`;
+			debug(`Requested player record not found. ID ${playerId}`);
+		}
+		else
+		{
+			res.status(200);
+			body = player;
+			debug(`Requested player found! Retrieved record for player with ID ${playerId}`);
+		}
+
+		return res.send(body);
 	},
 
 	update: async (req: Request, res: Response) =>
 	{
-		// todo
-		return res.status(200);
+		const playerId = parseInt(req.params.id);
+		const update = req.body;
+
+		const result = await updatePlayer(playerId, update);
+
+		let body: any = null;
+
+		if (result.modifiedCount < 1)
+		{
+			res.status(400);
+			body = 'Error updating player information: No documents modified';
+		}
+		else
+		{
+			res.status(200);
+			body = 'Player record updated!';
+		}
+
+		return res.send(body);
 	},
 
 	delete: async (req: Request, res: Response) =>
 	{
-		// todo
-		return res.status(200);
+		const playerId = parseInt(req.params.id);
+		let body: any = null;
+
+		const result = await deletePlayer(playerId);
+
+		if (result.deletedCount > 0)
+		{
+			res.status(200);
+			body = `Player with ID ${playerId} deleted`;
+		}
+		else
+		{
+			res.status(404);
+			body = `Unable to delete player with ID ${playerId}`;
+		}
+		
+		return res.send(body);
 	}
 }
--- src/routers/player.ts
+++ src/routers/player.ts
@@ -1,15 +1,16 @@
-import express, { Request, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import passport from '../config/passport-config.js';
 
 import controller from '../controllers/player.js';
 
 const router = express.Router();
-const auth = passport.authenticate('jwt', { session: false });
+//const auth = passport.authenticate('jwt', { session: false });
 
-router.route('/new')
-	.post(auth, controller.create);
+//test auth
+const auth = async (req: Request, res: Response, done: Function) => {done(null, false)};
 
 router.route('/:id')
+	.post(auth, controller.create)
 	.get(auth, controller.read)
 	.put(auth, controller.update)
 	.delete(auth, controller.delete);
